c9aca06556c88b72ab70e115303ac6a6aad6a6f7
musa <musasorkar95@gmail.com>
2025-05-13 22:58:28 +0600

    Initial commit

---
+++ bin/dart_all.dart
@@ -0,0 +1,85 @@
+void main() {
+
+  // Groabole List
+//   var city =['Dhaka','Gaibadha','Rnagpur'];
+//     print(city);
+//   city.add("Sundarganj");
+//   print(city);
+
+
+  // fixed length list
+//   const city =['Dhaka','Gaibandha','Rangpur'];
+//   print(city);
+
+  //List Insert 
+  // add ,addAll,Insert, insartAll
+//   var numbers =[1,2,3,4];
+//    print(numbers);
+//   numbers.insertAll(2,[100,200,300,400]);
+//     print(numbers);
+
+
+  //list update and remove
+
+//   var numbers =[1,2,3,4];
+//    print(numbers);
+//   numbers[1]=200;
+//   numbers[2]=300;
+//   numbers[3]=400;
+//     print(numbers);
+
+//    var numbers =[1,2,3,4];
+//    print(numbers);
+//   numbers.remove(2);
+//     print(numbers);
+
+
+
+  // set declear ?
+  // add, addAll
+//   var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+//   myCitySet.addAll({"Gaibandha","Borisal"});
+//   print(myCitySet);
+  // set element add 
+//    var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+//   myCitySet.addAll({"Gaibandha","Borisal"});
+
+
+//   print(myCitySet.elementAt(4));
+
+
+  // set remove
+//    var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+
+//   myCitySet.clear();
+//   print(myCitySet);
+
+  // Set Properties length, inEmpty,IsNotEmpty,Single,last,first,Hashcode,
+
+//      var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+
+//   print(myCitySet.hashCode);
+
+  //Map key value add korbo kivabe
+  //cirlibrase use map
+
+//   var student ={
+//     'nmae':'musa',
+//     'age':40,
+//     'city':'Rnagpur'};
+
+//   student['country']='Bangladesh';
+
+//   print(student);
+
+  // constructor use map
+  // Map object making
+//key ,inEmpty,values,length,isNotEmpty
+  var person = new Map();
+  person['name']='Musa';
+  person['age']=20;
+  person['city']='Rangpur';
+
+  print(person.isNotEmpty);
+
+}
