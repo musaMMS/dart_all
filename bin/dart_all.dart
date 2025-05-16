40d41875652a97451f993ae2cfe3b08df7e2c2bf
musa <musasorkar95@gmail.com>
2025-05-16 23:43:19 +0600

    u

--- bin/dart_all.dart
+++ bin/dart_all.dart
@@ -1,9 +1,188 @@
-// function call
- addtowNumber(){
-  var x=10;
-  var y=200;
-  print(x+y);
- }
 void main() {
-  addtowNumber();
+//
+//   // Groabole List
+// //   var city =['Dhaka','Gaibadha','Rnagpur'];
+// //     print(city);
+// //   city.add("Sundarganj");
+// //   print(city);
+//
+//
+//   // fixed length list
+// //   const city =['Dhaka','Gaibandha','Rangpur'];
+// //   print(city);
+//
+//   //List Insert
+//   // add ,addAll,Insert, insartAll
+// //   var numbers =[1,2,3,4];
+// //    print(numbers);
+// //   numbers.insertAll(2,[100,200,300,400]);
+// //     print(numbers);
+//
+//
+//   //list update and remove
+//
+// //   var numbers =[1,2,3,4];
+// //    print(numbers);
+// //   numbers[1]=200;
+// //   numbers[2]=300;
+// //   numbers[3]=400;
+// //     print(numbers);
+//
+// //    var numbers =[1,2,3,4];
+// //    print(numbers);
+// //   numbers.remove(2);
+// //     print(numbers);
+//
+//
+//
+//   // set declear ?
+//   // add, addAll
+// //   var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+// //   myCitySet.addAll({"Gaibandha","Borisal"});
+// //   print(myCitySet);
+//   // set element add
+// //    var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+// //   myCitySet.addAll({"Gaibandha","Borisal"});
+//
+//
+// //   print(myCitySet.elementAt(4));
+//
+//
+//   // set remove
+// //    var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+//
+// //   myCitySet.clear();
+// //   print(myCitySet);
+//
+//   // Set Properties length, inEmpty,IsNotEmpty,Single,last,first,Hashcode,
+//
+// //      var myCitySet =<String>{'Dhaka','Rangpur,Bogura'};
+//
+// //   print(myCitySet.hashCode);
+//
+//   //Map key value add korbo kivabe
+//   //cirlibrase use map
+//
+// //   var student ={
+// //     'nmae':'musa',
+// //     'age':40,
+// //     'city':'Rnagpur'};
+//
+// //   student['country']='Bangladesh';
+//
+// //   print(student);
+//
+//   // constructor use map
+//   // Map object making
+// //key ,inEmpty,values,length,isNotEmpty
+// //   var person = new Map();
+// //   person['name']='Musa';
+// //   person['age']=20;
+// //   person['city']='Rangpur';
+// //
+// //   print(person.isNotEmpty);
+
+
+// if else use result
+//   var marks =30;
+//   if (marks >=80){
+//     print("A+");
+//   }
+//   else if (marks <90 && marks >=70){
+//     print("A-");
+//   }
+//   else if (marks <70 && marks >=60){
+//     print("A");
+//   }
+//   else if (marks <60 && marks >=50){
+//     print("B");
+//   }
+//   else if (marks <50 && marks >=40){
+//     print("C");
+//   }
+//   else if (marks <40 && marks >=33){
+//     print("D");
+//   }
+//   else {
+//     print("o");
+//   }
+//
+  // switch case use result
+
+  // var marks =55;
+  // switch(marks){
+  //   case  80 :
+  //   print("A+");
+  //       break;
+  //
+  //   case  80 :
+  //     print("A+");
+  //     break;
+  //   case  70 :
+  //     print("A-");
+  //     break;
+  //   case  60 :
+  //     print("A");
+  //     break;
+  //
+  //   case  50 :
+  //     print("B");
+  //     break;
+  //
+  //   case  40 :
+  //     print("c+");
+  //     break;
+  //   case  33 :
+  //     print("D");
+  //     break;
+  //   default:print("F");
+  //   break;
+  //
+  //
+  //
+  // }
+  // loop for
+  // for(var i =0;i<100;i=i+2){
+  //   print("Dart"+i.toString());
+  // }
+  //for imn loop
+  // list and arry te for in loop use kora hoi
+  // var AlphabetList=['a','d','f','g'];
+  // for(var OneAlphabet in AlphabetList){
+  //   print(OneAlphabet);
+  // }
+  // for in loop set er upor
+  // var AlphabetSet= {'a', 'd', 'f', 'g'};
+  // for(var OneAlphabet in AlphabetSet){
+  //   print(OneAlphabet);
+  // }
+  // //for in loop json
+  // var productList =[
+  //   {'name':'sopa','price':100},
+  //   {'name':'dim','price':50},
+  //   {'name':'misty','price':120},
+  //   {'name':'cake','price':200},
+  //   {'name':'milk','price':300},
+  // ];
+  // for (var OneProduct in productList){
+  //   print(OneProduct['price']);
+  //
+  // }
+  //while and do wile loop
+  //entry cntrol loop tar agei porichoy dite hoi
+  /*var i =0;
+  while(i<100){
+    print(i);
+    i=i+1;
+  }
+  */
+   // do wile loop
+  // do mins tmi age kaj koro then ami tmke next condition dici just wait
+  /*var i= 0;
+  do{
+    print(i);
+    i=i+1;
+  }while(i<100);
+   */
+  //Function part
 }
