9b51e8927d90db11c96d78ea00f32eefd28ad826
musa <musasorkar95@gmail.com>
2025-05-16 23:32:59 +0600

    u

--- bin/dart_all.dart
+++ bin/dart_all.dart
@@ -109,36 +109,81 @@ void main() {
 //
   // switch case use result
 
-  var marks =55;
-  switch(marks){
-    case  80 :
-    print("A+");
-        break;
-
-    case  80 :
-      print("A+");
-      break;
-    case  70 :
-      print("A-");
-      break;
-    case  60 :
-      print("A");
-      break;
-
-    case  50 :
-      print("B");
-      break;
-
-    case  40 :
-      print("c+");
-      break;
-    case  33 :
-      print("D");
-      break;
-    default:print("F");
-    break;
-
-
-
+  // var marks =55;
+  // switch(marks){
+  //   case  80 :
+  //   print("A+");
+  //       break;
+  //
+  //   case  80 :
+  //     print("A+");
+  //     break;
+  //   case  70 :
+  //     print("A-");
+  //     break;
+  //   case  60 :
+  //     print("A");
+  //     break;
+  //
+  //   case  50 :
+  //     print("B");
+  //     break;
+  //
+  //   case  40 :
+  //     print("c+");
+  //     break;
+  //   case  33 :
+  //     print("D");
+  //     break;
+  //   default:print("F");
+  //   break;
+  //
+  //
+  //
+  // }
+  // loop for
+  // for(var i =0;i<100;i=i+2){
+  //   print("Dart"+i.toString());
+  // }
+  //for imn loop
+  // list and arry te for in loop use kora hoi
+  // var AlphabetList=['a','d','f','g'];
+  // for(var OneAlphabet in AlphabetList){
+  //   print(OneAlphabet);
+  // }
+  // for in loop set er upor
+  // var AlphabetSet= {'a', 'd', 'f', 'g'};
+  // for(var OneAlphabet in AlphabetSet){
+  //   print(OneAlphabet);
+  // }
+  // //for in loop json
+  // var productList =[
+  //   {'name':'sopa','price':100},
+  //   {'name':'dim','price':50},
+  //   {'name':'misty','price':120},
+  //   {'name':'cake','price':200},
+  //   {'name':'milk','price':300},
+  // ];
+  // for (var OneProduct in productList){
+  //   print(OneProduct['price']);
+  //
+  // }
+  //while and do wile loop
+  //entry cntrol loop tar agei porichoy dite hoi
+  /*var i =0;
+  while(i<100){
+    print(i);
+    i=i+1;
   }
+  */
+   // do wile loop
+  // do mins tmi age kaj koro then ami tmke next condition dici just wait
+  /*var i= 0;
+  do{
+    print(i);
+    i=i+1;
+  }while(i<100);
+   */
+  //Function part
+
 }
